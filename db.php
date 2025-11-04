900fe1f7465b026eeaa18f7113b46a7a8aa6ee02
Mishaal <mishaalcanvawali1@gmail.com>
2025-11-04 09:46:29 +0500

    Create db.php

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "unzfutgwrtkyb";
+$pass = "zxqqgfz2ugxg";
+$dbname = "dbcwvv3be0miqt";
+
+$conn = mysqli_connect($host, $user, $pass, $dbname);
+
+if (!$conn) {
+    die("Database Connection Failed: " . mysqli_connect_error());
+}
+?>
